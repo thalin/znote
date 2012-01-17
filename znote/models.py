135b10d7fbe92bf40656db6878c79dfdca4b0eb6
Zeke Harris <thalin@gmail.com>
2012-01-17 16:21:12 -0500

    Added NoteBase class.
    Inherit NoteBase into Note and Task models.
    
    Move methods around as they make sense.

--- znote/models.py
+++ znote/models.py
@@ -9,18 +9,15 @@ import markdown
 
 class Notebook(tmodels.MPTTModel, emodels.TitleSlugDescriptionModel):
     parent = tmodels.TreeForeignKey('self', null=True, blank=True, related_name='children')
-    owner = models.ForeignKey('auth.User')
 
     def __str__(self):
         return self.title
 
-class Note(tmodels.MPTTModel, emodels.TimeStampedModel):
+class NoteBase(tmodels.MPTTModel, emodels.TimeStampedModel):
     title = models.CharField(max_length=255, blank=True)
-    slug = emodels.AutoSlugField(populate_from='title', blank=True)
-    text = models.TextField()
+    slug = models.emodels.AutoSlugField(populate_from='title', blank=True)
+    notebook = models.ForeignKey(Notebook, null=True, blank=True, related_name='items')
     parent = tmodels.TreeForeignKey('self', null=True, blank=True, related_name='children')
-    notebook = models.ForeignKey(Notebook, null=True, blank=True)
-    owner = models.ForeignKey('auth.User', editable=False, related_name='own_notes')
 
     class MPTTMeta:
         order_insertion_by = ['title']
@@ -30,6 +27,14 @@ class Note(tmodels.MPTTModel, emodels.TimeStampedModel):
             return "%s/%s" % (self.parent, self.title)
         else:
             return self.title
+
+    def note_children(self):
+        if self.children.count() <= 0:
+            return None
+        return self.children.filter()
+
+class Note(NoteBase):
+    text = models.TextField()
 
     def edit(self):
         fid, tmpfile = mkstemp(suffix='.note')
@@ -49,33 +54,17 @@ class Note(tmodels.MPTTModel, emodels.TimeStampedModel):
     def get_absolute_url(self):
         return('note_detail', [self.id])
 
-    @classmethod
-    def wikilinks_build_url(cls, label, base, end):
-        q = Q(title__iexact=label)|Q(slug__iexact=label)
-        try:
-            url = cls.objects.get(q).get_absolute_url()
-            url += '?title=%s' % label
-        except:
-            url = reverse('note_create')
-            url += '?title=%s' % label
-        return url
-
     def render_text(self):
-        md_ext = ['codehilite', 'wikilinks', 'toc', 'nl2br']
-        md_conf = {'wikilinks': [('build_url', self.wikilinks_build_url)]}
-        md = markdown.Markdown(extensions=md_ext, extension_configs=md_conf)
-        return md.convert(self.text)
+        pass
+        #md_ext = ['codehilite', 'wikilinks', 'toc', 'nl2br']
+        #md_conf = {'wikilinks': [('build_url', self.wikilinks_build_url)]}
+        #md = markdown.Markdown(extensions=md_ext, extension_configs=md_conf)
+        #return md.convert(self.text)
 
 STATUSES = ['New', 'In Progress', 'Finished', 'Expired']
 
 STATUS_CHOICES = [(n, choice) for n, choice in enumerate(STATUSES)]
 
-#Having Task inherit from Note is probably a bad idea.
-
-class Task(tmodels.MPTTModel, emodels.TimeStampedModel, emodels.TitleSlugDescriptionModel):
-    notebook = models.ForeignKey(Notebook, blank=True, null=True)
-    note = models.ForeignKey(Note, blank=True, null=True)
+class Task(NoteBase):
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     due = models.DateField(null=True, blank=True)
-    parent = tmodels.TreeForeignKey('self', null=True, blank=True, related_name='children')
-    owner = models.ForeignKey('auth.User', editable=False, related_name='own_tasks')
